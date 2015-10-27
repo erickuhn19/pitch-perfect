107e898e74da67bdab7a68b18bc43d25f41d13c9
Eric Kuhn <erickuhn19@gmail.com>
2015-10-26 21:33:06 -0400

    Embed navigation controller. Add new view in storyboard. Create new PlaySound Controller for playing audio. Add icon for playing audio slowly. Add Forest Gump box of chocolates mp3 using AVAudioPlayer. Press slow icon button and plays Forrest.

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,51 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Eric Kuhn on 10/25/15.
+//  Copyright © 2015 Eric Kuhn. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    var audioPlayer:AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        if let filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            let url = NSURL.fileURLWithPath(filePath)
+            audioPlayer = try!
+                AVAudioPlayer(contentsOfURL: url)
+        } else {
+            print("the filepath is empty")
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playSlow(sender: UIButton) {
+        audioPlayer.play()
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
