3c4c81f4eab5569043bc4de04224ff042978f086
Eric Kuhn <erickuhn19@gmail.com>
2015-10-30 13:49:00 -0400

    Changed audio speed to half. Add ability to record audio and stop recording through AVAudioRecorder. Change to record only on one file for performance. Add a new class called RecordedAudio.

---
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -0,0 +1,77 @@
+//
+//  RecordSoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Eric Kuhn on 10/22/15.
+//  Copyright © 2015 Eric Kuhn. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+
+    var audioRecorder:AVAudioRecorder!
+    
+    @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        stopButton.hidden = true
+        recordButton.enabled = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func stopRecording(sender: UIButton) {
+        recordingInProgress.hidden = true
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+
+    
+    @IBAction func recordAudio(sender: UIButton) {
+        stopButton.hidden = false
+        recordingInProgress.hidden = false
+        recordButton.enabled = false
+        //TODO: record the users voice
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        let recordingName = "my_audio.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+    }
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        //TODO: step 1 save the recorded audio
+        
+        
+        //TODO: step 2 move to the next scene aka perform seugue
+    }
+
+}
+
---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Eric Kuhn on 10/30/15.
+//  Copyright © 2015 Eric Kuhn. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject {
+    var filePathUrl: NSURL!
+    var title: String!
+}
