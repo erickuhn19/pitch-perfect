8bb5270218bb88ca42bd697b87b9a9021cb81625
Eric Kuhn <erickuhn19@gmail.com>
2015-10-31 14:11:02 -0400

    Task 3, removed legacy code that was commented out.

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -18,14 +18,6 @@ class PlaySoundsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-//        if let filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
-//            let url = NSURL.fileURLWithPath(filePath)
-//
-//        } else {
-//            print("the filepath is empty")
-//        }
         
         audioPlayer = try!
             AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl)
@@ -97,14 +89,6 @@ class PlaySoundsViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
